28af4373a6031848b5550837a4625c66b6b1372d
makkapakka0 <liyunfan199902@outlook.com>
2025-11-10 15:09:00 +0000

    Update TellusCd_res.R

--- TellusCd_res.R
+++ TellusCd_res.R
@@ -7,41 +7,49 @@ library(data.table)
 library(foreach)
 library(doParallel)
 
-
+#RF
 df<-read.csv('D:\\telluscd\\all3.csv')
 
-rf_g<-randomForest(Cd~tp+tem+road+industry+quarry+PIPP+soiltype+br+lc+LOI+pH+ele,
-                   ntree=350,
-                   mtry=3,
-                   data=df)
+rf<-randomForest(Cd~tp+tem+quarry+industry+road+PIPP+soiltype+lc+br+pH+LOI+ele,
+                 ntree=350,
+                 mtry=3,
+                 data=df)
 
-predg<-as.data.frame(predict(rf_g,df))
-res<-predg-df$Cd
-names(res)<-c('res')
+#Preparation for residual analysis
+pred<-predict(rf,df)
+df$res<-pred-df$Cd
 
-densityplot<-data.frame(df$Cd,rf[["predicted"]])
-names(densityplot)<-c('Observed','Predicted')
+res<-as.data.frame(df$res)
 
-ggplot(densityplot,aes(x=Observed,y=Predicted))+
-  geom_hex(bins=100)+
-  scale_fill_gradientn(
-    colors = c('#4B0082','blue','green','yellow','orange','red'),
-    values = scales::rescale(c(0,0.01,0.05,0.2,0.4,1)),
-    name='Density'
+res<-res%>%
+  mutate(bin=cut(df$res,breaks = seq(floor(min(df$res)),ceiling(max(df$res)),by=0.1)))%>%
+  count(bin)%>%
+  mutate(
+    bin_center=as.numeric(sub('\\((.+),.*','\\1',bin))+0.05,
+    log_count=log10(n+1)
+  )
+breaks_raw<-c(1,10,100,1000)
+breaks_log<-log10(breaks_raw+1)
+
+ggplot(res,aes(x=bin_center,y=log_count))+
+  geom_col(width = 0.1,fill='steelblue')+
+  xlab('Residuals')+
+  ylab('Count')+
+  scale_y_continuous(
+    breaks = breaks_log,
+    labels = breaks_raw
   )+
-  geom_abline(slope = 1,intercept = 0,linetype='dashed',color='red')+
-  xlim(0,5)+
-  ylim(0,5)+
-  xlab('Observed Cd (mg/kg)')+
-  ylab('Predicted Cd (mg/kg)')+
-  theme(axis.text = element_text(size=24),
-        axis.title = element_text(size=28),
-        panel.border = element_rect(colour = 'black',fill=NA,size=2),
-        panel.grid.major = element_line(colour = 'gray80'),
-        panel.grid.minor = element_line(colour = 'gray80'),
-        axis.line = element_line(colour = 'black'))+
-  ggsave('D:\\telluscd\\maps\\density.tiff',dpi=300,width=10,height=6)
+  theme_minimal()+
+  theme(axis.text = element_text(size=18),
+        axis.title = element_text(size=24),
+        panel.border = element_rect(colour = 'black',fill=NA,size=2))+
+  ggsave('D:\\telluscd\\maps\\gres.tiff',dpi=300,width=10,height=2)
 
+output<-st_as_sf(df,coords=c('x','y'),crs=2157)
+st_write(output,'D:\\telluscd\\working\\final\\res_g.shp',append=FALSE)
+
+#GWRF
+df<-read.csv('D:\\telluscd\\all3.csv')
 
 setDT(df)
 setkey(df, x, y)
@@ -49,61 +57,242 @@ numCores <- detectCores() - 1
 cl <- makeCluster(numCores)
 registerDoParallel(cl)
 
-pred4000 <- foreach(i = 1:nrow(df), .combine = rbind, .packages = c('randomForest', 'data.table')) %dopar% {
+#Parallel
+results <- foreach(i = 1:nrow(df), .combine = rbind, .packages = c('randomForest', 'data.table','fastshap')) %dopar% {
   row <- df[i]
   long1 <- row$x
   lat1 <- row$y
   
-  window_data <- df[sqrt((x-long1)^2+(y-lat1)^2)<4000]
-  sample_weight<-(1-(sqrt((window_data$x-long1)^2+(window_data$y-lat1)^2)/4000)^2)^2
+  window_data <- df[sqrt((x-long1)^2+(y-lat1)^2)<9000]
+ 
+  sample_weight<-(1-(sqrt((window_data$x-long1)^2+(window_data$y-lat1)^2)/9000)^2)^2
   
-  local <- randomForest(Cd~tp+tem+quarry+industry+road+PIPP+soiltype+lc+br+LOI+pH+ele,
+  local <- randomForest(Cd~tp+tem+quarry+industry+road+PIPP+soiltype+lc+br+pH+LOI+ele,
                         ntree=350,
                         mtry=3,
                         weights=sample_weight,
                         data = window_data)
   pred<-predict(local,row)
-  
   data.table(pred = pred)
 }
 
 stopCluster(cl)
 
-results_p<-cbind(pred4000,pred4500,pred5000,pred5500,pred6000,pred6500,pred7000,
-                 pred7500,pred8000,pred8500,pred9000,pred10000,pred15000,pred20000,
-                 pred25000,pred30000,pred35000,pred40000)
+#Residual
+df$res<-results$pred-df$Cd
 
-results_p<-results_p-df$Cd
+df_sf<-st_as_sf(df,coords=c('x','y'), crs=2157)
+coords<-st_coordinates(df_sf)
+nb<-dnearneigh(coords,0,3693)
+dist_mat<-nbdists(nb,coords)
+inv_dist<-lapply(dist_mat, function(x) 1/x)
+lw<-nb2listw(nb,glist = inv_dist,style = 'W', zero.policy = TRUE)
+moran_res<-moran.test(df_sf$res,lw,zero.policy = TRUE)
+print(moran_res)
 
-names(results_p)<-c('pred4000','pred4500','pred5000','pred5500','pred6000',
-                    'pred6500','pred7000','pred7500','pred8000','pred8500','pred9000',
-                    'pred10000','pred15000','pred20000','pred25000','pred30000',
-                    'pred35000','pred40000')
+res<-as.data.frame(df$res)
+res<-res%>%
+  mutate(bin=cut(df$res,breaks = seq(floor(min(df$res)),ceiling(max(df$res)),by=0.1)))%>%
+  count(bin)%>%
+  mutate(
+    bin_center=as.numeric(sub('\\((.+),.*','\\1',bin))+0.05,
+    log_count=log10(n+1)
+  )
+breaks_raw<-c(1,10,100,1000)
+breaks_log<-log10(breaks_raw+1)
 
-merge<-cbind(df,results_p)
-
-output<-st_as_sf(merge,coords=c('x','y'),crs=2157)
-st_write(output,'D:\\telluscd\\working\\res_g.shp',append=FALSE)
-
-densityplot<-data.frame(df$Cd,results)
-names(densityplot)<-c('Observed','Predicted')
-
-ggplot(densityplot,aes(x=Observed,y=Predicted))+
-  geom_hex(bins=100)+
-  scale_fill_gradientn(
-    colors = c('#4B0082','blue','green','yellow','orange','red'),
-    values = scales::rescale(c(0,0.01,0.05,0.2,0.4,1)),
-    name='Density'
+ggplot(res,aes(x=bin_center,y=log_count))+
+  geom_col(width = 0.1,fill='steelblue')+
+  xlab('Residuals')+
+  ylab('Count')+
+  scale_y_continuous(
+    breaks = breaks_log,
+    labels = breaks_raw
   )+
-  geom_abline(slope = 1,intercept = 0,linetype='dashed',color='red')+
-  xlim(-3,3)+
-  ylim(-3,3)+
-  xlab('Observed Cd (mg/kg)')+
-  ylab('Predicted Cd (mg/kg)')+
+  theme_minimal()+
+  theme(axis.text = element_text(size=18),
+        axis.title = element_text(size=24),
+        panel.border = element_rect(colour = 'black',fill=NA,size=2))+
+  ggsave('D:\\telluscd\\maps\\lres.tiff',dpi=300,width=10,height=2)
+
+output<-st_as_sf(df,coords=c('x','y'),crs=2157)
+st_write(output,'D:\\telluscd\\working\\final\\res_l.shp',append=FALSE)
+
+
+
+
+#GWRF-Z
+df<-read.csv('D:\\telluscd\\all3.csv')
+
+setDT(df)
+setkey(df, x, y)
+numCores <- detectCores() - 1
+cl <- makeCluster(numCores)
+registerDoParallel(cl)
+
+#Parallel
+results <- foreach(i = 1:nrow(df), .combine = rbind, .packages = c('randomForest', 'data.table','fastshap')) %dopar% {
+  row <- df[i]
+  long1 <- row$x
+  lat1 <- row$y
+  
+  window_data <- df[sqrt((x-long1)^2+(y-lat1)^2)<9000]
+  window_data$z_scores <- scale(log(window_data$Cd))
+  window_data <- window_data[abs(window_data$z_scores) <= 3, ]
+  
+  sample_weight<-(1-(sqrt((window_data$x-long1)^2+(window_data$y-lat1)^2)/9000)^2)^2
+  
+  local <- randomForest(Cd~tp+tem+quarry+industry+road+PIPP+soiltype+lc+br+pH+LOI+ele,
+                        ntree=350,
+                        mtry=3,
+                        weights=sample_weight,
+                        data = window_data)
+  pred<-predict(local,row)
+  data.table(pred = pred)
+}
+
+stopCluster(cl)
+
+df$res<-results$pred-df$Cd
+
+df_sf<-st_as_sf(df,coords=c('x','y'), crs=2157)
+coords<-st_coordinates(df_sf)
+nb<-dnearneigh(coords,0,3693)
+dist_mat<-nbdists(nb,coords)
+inv_dist<-lapply(dist_mat, function(x) 1/x)
+lw<-nb2listw(nb,glist = inv_dist,style = 'W', zero.policy = TRUE)
+moran_res<-moran.test(df_sf$res,lw,zero.policy = TRUE)
+print(moran_res)
+
+res<-as.data.frame(df$res)
+
+res<-res%>%
+  mutate(bin=cut(df$res,breaks = seq(floor(min(df$res)),ceiling(max(df$res)),by=0.1)))%>%
+  count(bin)%>%
+  mutate(
+    bin_center=as.numeric(sub('\\((.+),.*','\\1',bin))+0.05,
+    log_count=log10(n+1)
+  )
+breaks_raw<-c(1,10,100,1000)
+breaks_log<-log10(breaks_raw+1)
+
+ggplot(res,aes(x=bin_center,y=log_count))+
+  geom_col(width = 0.1,fill='steelblue')+
+  xlab('Residuals')+
+  ylab('Count')+
+  scale_y_continuous(
+    breaks = breaks_log,
+    labels = breaks_raw
+  )+
+  theme_minimal()+
+  theme(axis.text = element_text(size=18),
+        axis.title = element_text(size=24),
+        panel.border = element_rect(colour = 'black',fill=NA,size=2))+
+  ggsave('D:\\telluscd\\maps\\lzres.tiff',dpi=300,width=10,height=2)
+
+output<-st_as_sf(df,coords=c('x','y'),crs=2157)
+st_write(output,'D:\\telluscd\\working\\final\\res_lz.shp',append=FALSE)
+
+
+#GWRF-LISA
+df<-read.csv('D:\\telluscd\\all3.csv')
+
+df_sf<-st_as_sf(df,coords = c('x','y'),crs=2157)
+#Local Moran's I
+coords<-st_coordinates(df_sf)
+nb<-dnearneigh(coords,d1=0,d2=9000)
+lw<-nb2listw(nb,style = 'W')
+local_moran<-localmoran(log(df_sf$Cd),lw)
+#Get I and p-value
+df$Ii<-local_moran[,'Ii']
+df$p<-local_moran[,5]
+
+setDT(df)
+setkey(df, x, y)
+numCores <- detectCores() - 1
+cl <- makeCluster(numCores)
+registerDoParallel(cl)
+
+results <- foreach(i = 1:nrow(df), .combine = rbind, .packages = c('randomForest', 'data.table','fastshap')) %dopar% {
+  row <- df[i]
+  long1 <- row$x
+  lat1 <- row$y
+
+  window_data <- df[sqrt((x-long1)^2+(y-lat1)^2)<9000]
+  window_data<-window_data[window_data$Ii>0 | window_data$p>0.05,]
+  
+  sample_weight<-(1-(sqrt((window_data$x-long1)^2+(window_data$y-lat1)^2)/9000)^2)^2
+
+  local <- randomForest(Cd~tp+tem+quarry+industry+road+PIPP+soiltype+lc+br+pH+LOI+ele,
+                        ntree=350,
+                        mtry=3,
+                        weights=sample_weight,
+                        data = window_data)
+  pred<-predict(local,row)
+  data.table(pred = pred)
+}
+
+stopCluster(cl)
+
+df$res<-results$pred-df$Cd
+
+df_sf<-st_as_sf(df,coords=c('x','y'), crs=2157)
+coords<-st_coordinates(df_sf)
+nb<-dnearneigh(coords,0,3693)
+dist_mat<-nbdists(nb,coords)
+inv_dist<-lapply(dist_mat, function(x) 1/x)
+lw<-nb2listw(nb,glist = inv_dist,style = 'W', zero.policy = TRUE)
+moran_res<-moran.test(df_sf$res,lw,zero.policy = TRUE)
+print(moran_res)
+
+res<-as.data.frame(df$res)
+
+res<-res%>%
+  mutate(bin=cut(df$res,breaks = seq(floor(min(df$res)),ceiling(max(df$res)),by=0.1)))%>%
+  count(bin)%>%
+  mutate(
+    bin_center=as.numeric(sub('\\((.+),.*','\\1',bin))+0.05,
+    log_count=log10(n+1)
+  )
+breaks_raw<-c(1,10,100,1000)
+breaks_log<-log10(breaks_raw+1)
+
+ggplot(res,aes(x=bin_center,y=log_count))+
+  geom_col(width = 0.1,fill='steelblue')+
+  xlab('Residuals')+
+  ylab('Count')+
+  scale_y_continuous(
+    breaks = breaks_log,
+    labels = breaks_raw
+  )+
+  theme_minimal()+
+  theme(axis.text = element_text(size=18),
+        axis.title = element_text(size=24),
+        panel.border = element_rect(colour = 'black',fill=NA,size=2))+
+  ggsave('D:\\telluscd\\maps\\lmres.tiff',dpi=300,width=10,height=2)
+
+output<-st_as_sf(df,coords=c('x','y'),crs=2157)
+st_write(output,'D:\\telluscd\\working\\final\\res_lm.shp',append=FALSE)
+
+#Plot
+moran<-read.csv('D:\\telluscd\\moran_res.csv')
+moran<-na.omit(moran)
+
+ggplot(moran,aes(x=band,y=Z.score,group=Group,color=Group))+
+  annotate('rect',xmin=-Inf,xmax=Inf,ymin=-2.58,ymax=2.58,fill='pink',alpha=0.7)+
+  geom_line(size=1.5)+
+  geom_point(color='black',size=2)+
+  labs(x='Bandwidth (m)',y='Global Morans I Z-score')+
+  ylim(-8,54)+
+  theme_minimal()+
   theme(axis.text = element_text(size=24),
         axis.title = element_text(size=28),
         panel.border = element_rect(colour = 'black',fill=NA,size=2),
         panel.grid.major = element_line(colour = 'gray80'),
         panel.grid.minor = element_line(colour = 'gray80'),
-        axis.line = element_line(colour = 'black'))+
-  ggsave('D:\\telluscd\\maps\\densitynstl.tiff',dpi=300,width=10,height=6)
+        axis.line = element_line(colour = 'black'),
+        legend.position = c(0.15,0.8),
+        legend.title = element_text(size=0),
+        legend.text = element_text(size=24))+
+  ggsave('D:\\telluscd\\maps\\moranres.tiff',dpi=300,width=10,height=8)
+
